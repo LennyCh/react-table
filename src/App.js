1122de1fa8de25e3578d1e4596110aa3350dd2f0
Lena Chepkasova <lena.chepkasova.95@mail.ru>
2020-07-28 23:16:59 +0300

    added search

--- src/App.js
+++ src/App.js
@@ -4,73 +4,109 @@ import Loader from './Loader/Loader';
 import Table from './Table/Table';
 import DetailRowView from './DetailRowView/DetailRowView';
 import ModeSelector from './ModeSelector/ModeSelector';
+import TableSearch from './TableSearch/TableSearch';
 import _ from 'lodash';
 
+
 class App extends Component {
-  state = {
+  state ={
     isModeSelected: false,
     isLoading: false,
     data: [],
-    sort: 'asc',
+    search: '',
+    sort: 'asc',  // 'desc'
     sortField: 'id',
     row: null,
     currentPage: 0,
-  };
-
+  }
   async fetchData(url) {
-    const response = await fetch(url);
-    const data = await response.json();
-    this.setState({
-      isLoading: false,
-      data: _.orderBy(data, this.state.sortField, this.state.sort)
-    })
-  };
+    const response = await fetch(url)
+    const data = await response.json()
 
+  this.setState({
+    isLoading: false,
+    data: _.orderBy(data, this.state.sortField, this.state.sort)
+  })
+
+  }
   onSort = sortField => {
     const cloneData = this.state.data.concat();
     const sort = this.state.sort === 'asc' ? 'desc' : 'asc';
     const data = _.orderBy(cloneData, sortField, sort);
-    this.setState({
-      data,
-      sort,
-      sortField
-    })
-  };
-
-  onRowSelect = row => (this.setState({row}));
+    this.setState({ data, sort, sortField })
+  }
 
   modeSelectHandler = url => {
+    // console.log(url)
     this.setState({
       isModeSelected: true,
       isLoading: true,
     })
-    this.fetchData(url);
-  };
+    this.fetchData(url)
+  }
 
-  pageChangeHandler = ({selected}) => (this.setState({currentPage: selected}));
+
+  onRowSelect = row => (
+    this.setState({row})
+  )
+
+  pageChangeHandler = ({selected}) => (
+    this.setState({currentPage: selected})
+  )
+
+  searchHandler = search => {
+    this.setState({search, currentPage: 0})
+  }
+
+  getFilteredData(){
+    const {data, search} = this.state
+
+    if (!search) {
+      return data
+    }
+    var result = data.filter(item => {
+      return (
+        item["firstName"].toLowerCase().includes(search.toLowerCase()) ||
+        item["lastName"].toLowerCase().includes(search.toLowerCase()) ||
+        item["email"].toLowerCase().includes(search.toLowerCase())
+      );
+    });
+    if(!result.length){
+      result = this.state.data
+    }
+    return result
+  }
 
   render() {
     const pageSize = 50;
     if(!this.state.isModeSelected){
       return (
         <div className="container">
-          <ModeSelector onSelect={this.modeSelectHandler} />
+          <ModeSelector onSelect={this.modeSelectHandler}/>
         </div>
       )
     }
-    const displayData = _.chunk(this.state.data, pageSize)[this.state.currentPage];
+
+    const filteredData = this.getFilteredData();
+    // debugger
+    const pageCount = Math.ceil(filteredData.length / pageSize)
+    const displayData = _.chunk(filteredData, pageSize)[this.state.currentPage]
     return (
       <div className="container">
       {
-        this.state.isLoading
+        this.state.isLoading 
         ? <Loader />
-        : <Table
-        data = {displayData}
-        onSort = {this.onSort}
-        sort = {this.state.sort}
-        sortField = {this.state.sortField}
-        onRowSelect={this.onRowSelect}
-        />
+        : <React.Fragment>
+          <TableSearch onSearch={this.searchHandler}/>
+            <Table 
+              data={displayData}
+              onSort={this.onSort}
+              sort={this.state.sort}
+              sortField={this.state.sortField}
+              onRowSelect={this.onRowSelect}
+            />
+          </React.Fragment>
+
       }
 
       {
@@ -80,7 +116,7 @@ class App extends Component {
         nextLabel={'>'}
         breakLabel={'...'}
         breakClassName={'break-me'}
-        pageCount={20}
+        pageCount={pageCount}
         marginPagesDisplayed={2}
         pageRangeDisplayed={5}
         onPageChange={this.pageChangeHandler}
@@ -92,7 +128,8 @@ class App extends Component {
         nextClassName="page-item"
         previousLinkClassName="page-link"
         nextLinkClassName="page-link"
-        /> : null
+        forcePage={this.state.currentPage}
+      /> : null
       }
 
       {
